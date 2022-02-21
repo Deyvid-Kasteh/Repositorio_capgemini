5dd867ff7cf103b61399ee294bdaab2d1c10fdd3
Deyvid Kasteh <99744584+Deyvid-Kasteh@users.noreply.github.com>
2022-02-20 23:58:48 -0300

    ultima alteração

--- Questao_1/script.js
+++ Questao_1/script.js
@@ -2,14 +2,15 @@ let inputdegraus = document.getElementById('degraus');
 let resultado = document.getElementById('resultado');
 
 function calcular() {
-    let a = " ";
+    let a = "&nbsp";
     let b = "*";
     var x = inputdegraus.value;
 
     let degraus = inputdegraus.value;
 
     for (let i = 1; i <= degraus; i++){
-    console.log(`${a.repeat(degraus - i)}`+`${b.repeat( degraus - (x - i))}`);
+    
+    resultado.innerHTML += `${a.repeat(degraus - i)}`+`${b.repeat( degraus - (x - i))}<br>`;
 
     }    
 
